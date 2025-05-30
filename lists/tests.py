09cdb5d412c85ad6d574bb7e145f105108a00bda
tasarma <tasarm7@protonmail.com>
2025-05-30 21:09:50 +0200

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -11,17 +11,17 @@ class HomePageTest(TestCase):
 
     def test_renders_input_content(self):
         response  = self.client.get("/")
-        self.assertContains(response, '<form method="POST">')
+        self.assertContains(response, '<form method="POST" action="/">')
         self.assertContains(response, '<input name="item_text"')
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-
-        response = self.client.get("/")
-
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
+    # def test_displays_all_list_items(self):
+    #     Item.objects.create(text="itemey 1")
+    #     Item.objects.create(text="itemey 2")
+    #
+    #     response = self.client.get("/")
+    #
+    #     self.assertContains(response, "itemey 1")
+    #     self.assertContains(response, "itemey 2")
 
     def test_can_save_a_POST_request(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
@@ -32,11 +32,30 @@ class HomePageTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/")
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_renders_input_form(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertContains(response, '<form method="POST" action="/">')
+        self.assertContains(response, '<input name="item_text"')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -56,12 +75,3 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(second_saved_item.text, "Item the second")
 
-    # def test_can_save_a_POST_request(self):
-    #     response = self.client.post("/", data={"item_text": "A new list item"})
-    #
-    #     self.assertEqual(Item.objects.count(), 1)  
-    #     new_item = Item.objects.first()  
-    #     self.assertEqual(new_item.text, "A new list item")  
-    #
-    #     self.assertContains(response, "A new list item")
-    #     self.assertTemplateUsed(response, "home.html")
