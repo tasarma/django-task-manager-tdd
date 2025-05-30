b48bd80676da75195df2c42e118097b3d947f173
tasarma <tasarm7@protonmail.com>
2025-05-30 23:17:41 +0200

    create a new list for every single new item submission

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -13,17 +13,6 @@ class HomePageTest(TestCase):
         response  = self.client.get("/")
         self.assertContains(response, '<form method="POST" action="/lists/new">')
         self.assertContains(response, '<input name="item_text"')
-
-    # def test_can_save_a_POST_request(self):
-    #     response = self.client.post("/", data={"item_text": "A new list item"})
-    #
-    #     self.assertEqual(Item.objects.count(), 1)
-    #     new_item = Item.objects.first()
-    #     self.assertEqual(new_item.text, "A new list item")
-
-    # def test_redirects_after_POST(self):
-    #     response = self.client.post("/", data={"item_text": "A new list item"})
-    #     self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
@@ -47,23 +36,27 @@ class ListViewTest(TestCase):
         self.assertContains(response, '<input name="item_text"')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        mylist = List.objects.create()
+        Item.objects.create(text="itemey 1", list=mylist)
+        Item.objects.create(text="itemey 2", list=mylist)
 
-        response = self.client.get("/lists/the-only-list-in-the-world/")
-
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
-
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        mylist = List()
+        mylist.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = mylist
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = mylist
         second_item.save()
+
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list, mylist)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -71,5 +64,7 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, mylist)
         self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, mylist)
 
