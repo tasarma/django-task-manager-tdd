2024f152f274da89ba4b3a84cf8ef5fa3bf4b6b7
tasarma <tasarm7@protonmail.com>
2025-05-30 14:29:47 +0200

    Can  add item

--- lists/tests.py
+++ lists/tests.py
@@ -10,5 +10,11 @@ class HomePageTest(TestCase):
 
     def test_renders_homepage_content(self):
         response  = self.client.get("/")
-        self.assertTemplateUsed(response, "To-Do")
+        self.assertContains(response, '<form method="POST">')
+        self.assertContains(response, '<input name="item_text"')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})   
+        self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response, "home.html")
 
