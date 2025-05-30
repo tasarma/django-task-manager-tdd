02e437f814786a417156e58103ad98dbbec96f53
tasarma <tasarm7@protonmail.com>
2025-05-30 16:54:11 +0200

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -5,19 +5,38 @@ from lists.models import Item
 
 
 class HomePageTest(TestCase):
-    def test_home_template(self):
+    def test_uses_home_template(self):
         response  = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_renders_homepage_content(self):
+    def test_renders_input_content(self):
         response  = self.client.get("/")
         self.assertContains(response, '<form method="POST">')
         self.assertContains(response, '<input name="item_text"')
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+
     def test_can_save_a_POST_request(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})   
-        self.assertContains(response, "A new list item")
-        self.assertTemplateUsed(response, "home.html")
+        response = self.client.post("/", data={"item_text": "A new list item"})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -36,3 +55,13 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(second_saved_item.text, "Item the second")
+
+    # def test_can_save_a_POST_request(self):
+    #     response = self.client.post("/", data={"item_text": "A new list item"})
+    #
+    #     self.assertEqual(Item.objects.count(), 1)  
+    #     new_item = Item.objects.first()  
+    #     self.assertEqual(new_item.text, "A new list item")  
+    #
+    #     self.assertContains(response, "A new list item")
+    #     self.assertTemplateUsed(response, "home.html")
