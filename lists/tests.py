bcf09ebaefe82953be0e0401698c1bb36f697c0b
tasarma <tasarm7@protonmail.com>
2025-05-31 15:04:03 +0200

    use lxml for more specific unit test asserts on html content

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 from lists.models import Item, List
+import lxml.html
 
 
 class HomePageTest(TestCase):
@@ -11,12 +12,11 @@ class HomePageTest(TestCase):
 
     def test_renders_input_form(self):
         response = self.client.get("/")
-        self.assertContains(response, '<form method="POST" action="/lists/new">')
-        self.assertContains(
-            response,
-            '<input name="item_text" id="id_new_item" placeholder="Enter a to-do item" />',
-            html=True,
-        )
+        parsed = lxml.html.fromstring(response.content)  
+        [form] = parsed.cssselect("form[method=POST]")   
+        self.assertEqual(form.get("action"), "/lists/new")
+        inputs = form.cssselect("input")
+        self.assertIn("item_text", [input.get("name") for input in inputs])
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
@@ -39,15 +39,12 @@ class ListViewTest(TestCase):
     def test_renders_input_form(self):
         mylist = List.objects.create()
         response = self.client.get(f"/lists/{mylist.id}/")
-        self.assertContains(
-            response,
-            f'<form method="POST" action="/lists/{mylist.id}/add_item">',
-        )
-        self.assertContains(
-            response,
-            '<input name="item_text" id="id_new_item" placeholder="Enter a to-do item" />',
-            html=True,
-        )
+        parsed = lxml.html.fromstring(response.content)  
+        [form] = parsed.cssselect("form[method=POST]")   
+        self.assertEqual(form.get("action"), f"/lists/{mylist.id}/add_item")
+        inputs = form.cssselect("input")
+
+        self.assertIn("item_text", [input.get("name") for input in inputs])
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()  
